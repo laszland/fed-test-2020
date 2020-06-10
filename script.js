7d53675ee5e13bf10306e9ee41b1553fe72d7570
Laszlo Roland Kiss <laszlo.roland.kiss@gmail.com>
2020-06-10 22:45:13 +0200

    create custom drop down

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".sort .default").click(function(){
+    $(".sort .select").toggleClass("active");
+  })
+
+  $(".filter .default").click(function(){
+    $(".filter .select").toggleClass("active");
+  })
+
+})
