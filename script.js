f61dd179e35666f38dbe1eb61b292ef75cb55a2f
Laszlo Roland Kiss <laszlo.roland.kiss@gmail.com>
2020-06-16 16:19:22 +0200

    reset grid base layout, add sort and filter function

--- script.js
+++ script.js
@@ -7,40 +7,69 @@ $(document).ready(function(){
     $(".filter .select").toggleClass("active");
   })
 
+  let url = new URL(window.location.href);
+  console.log(url);
+
   $(".sort .select li").click(function(){
-    var currentele = $(this).html();
-    $(".sort .default li").html(currentele);
+    let current = $(this).html();
+    let order = $(this).children().attr("key");
+    url.searchParams.set('sort', order);
+    window.location = url.href;
+    console.log(url);
+    $(".sort .default li").html(current);
     $(".sort .select").removeClass("active");
   })
 
   $(".filter .select li").click(function(){
-    var currentele = $(this).html();
-    $(".filter .default li").html(currentele);
+    var current = $(this).html();
+    let filter = $(this).children().attr("key");
+    url.searchParams.set('filter', filter);
+    window.location = url.href;
+    console.log(url);
+    $(".filter .default li").html(current);
     $(".filter .select").removeClass("active");
   })
 
   let products;
   let productCounter = 0;
 
+  function compare(order = 'asc') {
+    return function innerSort(a, b) {
+      console.log(order);
+      let comparision = 0;
+      if (a.price > b.price) {
+        comparision = 1;
+      } else if (a.price < b.price) {
+        comparision = -1;
+      }
+      return (
+        (order === 'desc') ? (comparision * -1) : comparision
+      );
+    }
+  }
+
   $.ajax("https://d2t3o0osqtjkex.cloudfront.net/tgTest/prods.json", {
     type: "GET",
     dataType: "json",
     success: function(data) {
-      products = data;
+      products = data.sort(compare(url.searchParams.get('sort')));
       products.map(item => {
         let wrapper = $(".wrapper");
-        wrapper.append(`
-        <article>
-          <img src=${item.image_link_mb} alt=${item.title}>
-          <div>
-            <p class="title">${item.title}</p>
-          </div>
-          <div class="prices">
-            <p class="crossed">$${item.crossed_price}</p>
-            <p>$${item.price}</p>
-          </div>
-        </article>
-        `)
+        let filter = url.searchParams.get('filter') ? url.searchParams.get('filter').toUpperCase() : "";
+        if (item.title.toUpperCase().includes(filter)) {
+          wrapper.append(`
+          <article>
+            <img src=${item.image_link_mb} alt=${item.title}>
+            <div>
+              <p class="title">${item.title}</p>
+            </div>
+            <div class="prices">
+              <p class="crossed">$${item.crossed_price}</p>
+              <p>$${item.price}</p>
+            </div>
+          </article>
+          `)
+        }
         productCounter++;
       });
       $(".filters > p").html(productCounter + ' Results')
